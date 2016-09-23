dd679ea68583d5259685807eb63a51bb34e7a252
robosoup <john.architect@gmail.com>
2016-09-23 12:48:55 +0100

    Multi-thread test function

--- Test.cs
+++ Test.cs
@@ -2,7 +2,10 @@
 // www.robosoup.com
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Athena
 {
@@ -18,38 +21,66 @@ namespace Athena
     internal class Test
     {
         private const string TestFile = "test.csv";
+        private Model model;
+        private int threadCount = 0;
+        private int correct;
+        private int count;
 
         public Test(Model model)
         {
-            Console.WriteLine("Performing test [{0:H:mm:ss}]", DateTime.Now);
+            this.model = model;
+            Console.WriteLine("Starting test [{0:H:mm:ss}]", DateTime.Now);
             Console.WriteLine();
             Console.WriteLine("Hit 'Esc' to quit test early...");
             Console.WriteLine();
 
-            double total = 0;
-            using (StreamReader r = new StreamReader(TestFile))
-                while (r.ReadLine() != null) { total++; }
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
 
             string line;
-            double count = 0;
-            var correct = 0;
+            var items = new List<string>();
+            using (StreamReader r = new StreamReader(TestFile))
+                while ((line = r.ReadLine()) != null)
+                    items.Add(line);
+            var total = items.Count;
+
             var start = DateTime.Now;
-            using (var sr = new StreamReader(TestFile))
-                while ((line = sr.ReadLine()) != null)
+            foreach (var item in items)
+            {
+                Task.Factory.StartNew(() => ProcessItem(item, token));
+                Interlocked.Increment(ref threadCount);
+
+                var seconds = (DateTime.Now - start).TotalSeconds + 1;
+                Console.Write("Progress: {0:0.000%}  items/sec: {1:0.00}  \r", (double)count / total, count / seconds);
+
+                if (Console.KeyAvailable && (Console.ReadKey(true).Key == ConsoleKey.Escape))
                 {
-                    count++;
-                    var keys = line.Split(',');
-                    var phrase = string.Format("{0}: {1} {2}", keys[0], keys[1], keys[2]);
-                    if (model.NearestWord(phrase) == keys[3]) correct++;
-                    var seconds = (DateTime.Now - start).TotalSeconds + 1;
-                    var rate = count / seconds;
-                    Console.Write("Progress: {0:0.000%}  items/sec: {1:0.00}  \r", count / total, rate);
-                    if (Console.KeyAvailable && (Console.ReadKey(true).Key == ConsoleKey.Escape)) break;
+                    tokenSource.Cancel();
+                    break;
                 }
 
+                while (threadCount > 99) Thread.Sleep(250);
+            }
+
+            while (threadCount > 0) Thread.Sleep(250);
+
             Console.WriteLine("\r\n");
-            Console.WriteLine("Accuracy = {0:0.000%}", correct / count);
+            Console.WriteLine("Accuracy = {0:0.000%}", (double)correct / count);
             Console.WriteLine();
+        }
+
+        private void ProcessItem(string item, CancellationToken ct)
+        {
+            if (ct.IsCancellationRequested)
+            {
+                Interlocked.Decrement(ref threadCount);
+                return;
+            }
+            var keys = item.Split(',');
+            var phrase = string.Format("{0}: {1} {2}", keys[0], keys[1], keys[2]);
+            if (model.NearestWord(phrase) == keys[3]) Interlocked.Increment(ref correct);
+            Interlocked.Increment(ref count);
+            Interlocked.Decrement(ref threadCount);
         }
     }
 }
